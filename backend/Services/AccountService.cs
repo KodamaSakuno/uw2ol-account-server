a3ead3f6db3a028006ac4a4f65864e285fa4bda8
神樹桜乃 <kodama@sakuno.moe>
2021-11-03 00:41:49 +0800

    Use rabbitmq instead

--- backend/Services/AccountService.cs
+++ backend/Services/AccountService.cs
@@ -1,33 +1,43 @@
 ﻿using Dapper;
 using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Caching.Distributed;
-using NATS.Client;
+using RabbitMQ.Client;
 using System.Text.Json;
 
 namespace Backend.Services;
 
 public class AccountService
 {
+    private readonly IConfiguration _configuration;
     private readonly IDistributedCache _cache;
-    private readonly SqliteConnection _connection;
-
-    private readonly Options _natsOptions;
+    private readonly SqliteConnection _sqliteConnection;
 
     private readonly object _registerLock = new();
 
+    private IConnection _rabbitMqConnection = default!;
+    private IModel _channel = default!;
+
     public AccountService(IConfiguration configuration, IDistributedCache cache)
     {
-        _connection = new(configuration.GetValue<string?>("ConnectionString") ?? throw new InvalidOperationException("Require connection string"));
+        _configuration = configuration;
+        _sqliteConnection = new(_configuration.GetValue<string?>("ConnectionString") ?? throw new InvalidOperationException("Require connection string"));
         _cache = cache;
-
-        _natsOptions = ConnectionFactory.GetDefaultOptions();
-        _natsOptions.Servers = new[] { configuration.GetValue<string?>("NatsEndpoint") ?? throw new InvalidOperationException("Require nats endpoint") };
     }
 
     public void Initialize()
     {
-        _connection.Open();
-        _connection.Execute("CREATE TABLE IF NOT EXISTS account(address TEXT PRIMARY KEY NOT NULL, name TEXT NOT NULL) WITHOUT ROWID;");
+        _sqliteConnection.Open();
+        _sqliteConnection.Execute("CREATE TABLE IF NOT EXISTS account(address TEXT PRIMARY KEY NOT NULL, name TEXT NOT NULL) WITHOUT ROWID;");
+
+        var rabbitMqFactory = new ConnectionFactory()
+        {
+            HostName = _configuration.GetValue<string?>("RabbitMQHost") ?? throw new InvalidOperationException("Require rabbitmq host")
+        };
+
+        _rabbitMqConnection = rabbitMqFactory.CreateConnection();
+
+        _channel = _rabbitMqConnection.CreateModel();
+        _channel.QueueDeclare("LoginResult", exclusive: false);
     }
 
     public async ValueTask<string?> GetNonceAsync(string address) => await _cache.GetStringAsync($"nonce:{address.ToLowerInvariant()}");
@@ -46,18 +56,16 @@ public class AccountService
     public void Register(string address, string name)
     {
         lock(_registerLock)
-            _connection.Execute("INSERT INTO account VALUES(@address, @name);", new { address = address.ToLowerInvariant(), name });
+            _sqliteConnection.Execute("INSERT INTO account VALUES(@address, @name);", new { address = address.ToLowerInvariant(), name });
     }
 
     public string? GetName(string address) =>
-        _connection.ExecuteScalar<string?>("SELECT name FROM account WHERE address = @address;", new { address = address.ToLowerInvariant() });
+        _sqliteConnection.ExecuteScalar<string?>("SELECT name FROM account WHERE address = @address;", new { address = address.ToLowerInvariant() });
 
     public void NotifyLogin(string session, string address, string name)
     {
-        using var connection = new ConnectionFactory().CreateConnection(_natsOptions);
-
         var json = JsonSerializer.SerializeToUtf8Bytes(new { session, address = address.ToLowerInvariant(), name });
 
-        connection.Publish("login_result", json);
+        _channel.BasicPublish(string.Empty, "LoginResult", body: json);
     }
 }
